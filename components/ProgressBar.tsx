24a5aaeba12687249970a00bbe06998c284f852f
Randy Harkediansa <randy.harkediansa@gmail.com>
2025-07-31 13:36:58 +0700

    add ProgressBar

---
+++ components/ProgressBar.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface ProgressBarProps {
+  current: number;
+  total: number;
+}
+
+const ProgressBar: React.FC<ProgressBarProps> = ({ current, total }) => {
+  const percentage = (current / total) * 100;
+
+  return (
+    <div className="w-full bg-slate-200 rounded-full h-2.5 mb-4">
+      <div
+        className="bg-sky-600 h-2.5 rounded-full transition-all duration-500 ease-out"
+        style={{ width: `${percentage}%` }}
+      ></div>
+    </div>
+  );
+};
+
+export default ProgressBar;
